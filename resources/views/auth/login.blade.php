efcfc4bd559c6d05b291fa9d41f73c59599d41d9
primmshadikusumah <primasurya200504@gmail.com>
2025-09-18 23:16:19 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+@extends('layouts.app')
+
+@section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -67,6 +69,12 @@
                                             {{ __('Forgot Your Password?') }}
                                         </a>
                                     @endif
+
+                                    <div class="mt-3">
+                                        <a href="{{ route('register') }}" class="btn btn-secondary">
+                                            {{ __('Register') }}
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -75,4 +83,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -30,10 +33,13 @@
         @endisset
 
         <!-- Page Content -->
-        <main>
+        <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
